4c1f4c202751c274f780ba3a90820e16b2b6e6f9
viethoang0207 <71074038+0968377398@users.noreply.github.com>
2021-10-15 20:12:05 +0700

    nopbai

---
+++ DemoT2009M/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoT2009M
+{
+    internal class Program
+    {
+        public static void demSo(int[] A, int[] B, int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (A[i] == B[j])
+                    {
+                        Console.WriteLine(A[i] + " ");
+                    }
+                }
+            }
+        }
+
+        public static void Main(String[] args){
+            int[] A ={1, 3, 4, 7, 13};
+            int[] B ={1, 2, 4, 13, 15};
+            demSo(A,B, A.Length, B.Length);
+        }
+    }
+}
